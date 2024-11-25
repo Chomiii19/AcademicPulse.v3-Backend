8916b28a71fe2b759a1f15a46d076a13268fed6e
Chomiii19 <jomariborines55@gmail.com>
2024-11-25 08:23:01 +0800

    update

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,17 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import StudentService from "../services/studentService";
+// import StudentService from "../services/studentService";
 import catchAsync from "../utils/catchAsync";
 
 const getAllStudents = catchAsync(async (req: Request, res: Response) => {
-  const { students, totalCount, totalPages } =
-    await StudentService.getAllStudentsQuery(req);
-
-  res.status(200).json({
-    status: "Success",
-    totalStudents: totalCount,
-    totalPages,
-    data: students,
-  });
+  // const { students, totalCount, totalPages } =
+  // await StudentService.getAllStudentsQuery(req);
+  // res.status(200).json({
+  //   status: "Success",
+  //   totalStudents: totalCount,
+  //   totalPages,
+  //   data: students,
+  // });
 });
 
 const getStudent = catchAsync(
