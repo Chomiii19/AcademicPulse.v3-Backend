fda39f5f0c9c0c100865d5c2fae4101bc8461a6c
Chomiii19 <jomariborines55@gmail.com>
2024-11-26 11:15:08 +0800

    update

--- src/utils/template/verifySchoolTemplate.ts
+++ src/utils/template/verifySchoolTemplate.ts
@@ -158,7 +158,7 @@ const schoolVerification = (email: string, token: string) => {
                       <table border="0" cellpadding="0" cellspacing="0">
                         <tr>
                           <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                            <a href="https://acadpulse-backend.onrender.com/verify-school/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify School</a>
+                            <a href="https://acadpulse-backend.onrender.com/api/v1/app/verify-school/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify School</a>
                           </td>
                         </tr>
                       </table>
