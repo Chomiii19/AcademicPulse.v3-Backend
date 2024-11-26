9e2b4f371a553d5e465f440867918bf9c482a21b
Chomiii19 <jomariborines55@gmail.com>
2024-11-26 10:57:28 +0800

    update

--- src/middlewares/uploadConfig.ts
+++ src/middlewares/uploadConfig.ts
@@ -3,12 +3,9 @@ import { fileURLToPath } from "url";
 import path from "path";
 import AppError from "../errors/appError";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve(__dirname, "../dev-data/data/"));
+    cb(null, path.resolve(process.cwd(), "dev-data/data/"));
   },
 
   filename: function (req, file, cb) {
