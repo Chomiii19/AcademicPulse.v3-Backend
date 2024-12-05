af3acf085498dc1b973db45bc03210d98769c76b
Chomiii19 <jomariborines55@gmail.com>
2024-12-05 21:15:51 +0800

    update

--- src/utils/verifyToken.ts
+++ src/utils/verifyToken.ts
@@ -1,7 +1,11 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
+import AppError from "../errors/appError";
 
 //@ts-expect-error
 const verifyToken = async (token: string): any => {
+  if (typeof token !== "string")
+    throw new AppError("Invalid token format", 400);
+
   return await jwt.verify(token, process.env.SECRET_KEY as string);
 };
 
