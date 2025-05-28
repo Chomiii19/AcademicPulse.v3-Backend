7798b723e2a14fc6b18d22f6a61757b2ae1ac542
Chomiii19 <jomariborines55@gmail.com>
2025-05-28 08:02:31 +0800

    updated cors

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app = express();
 app.set("trust proxy", 1);
 app.use(
   cors({
-    origin: "*",
+    origin: [`${process.env.APP_ORIGIN}`, "http://localhost:5173"],
     credentials: true,
   })
 );
